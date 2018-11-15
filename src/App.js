f4dee192925bfa4bc74c31d03d9599b30d7b573e
Yura-PC\Yura <yuraost01@gmail.com>
2018-11-15 13:51:32 +0200

    no message

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootswatch/dist/journal/bootstrap.css";
+
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
+
+const PLACES = [
+  { name: "Kiev", city: "Kiev,ua" },
+  { name: "Lviv", city: "Lviv,ua" },  
+  { name: "Skole", city: "Skole,ua" },
+  { name: "Odessa", city: "Odessa,ua" }
+];
+
+class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+  componentDidMount() {
+    const city = this.props.city;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json });
+    });
+  }
+  render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0
+    };
+  }
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              React Simple Weather App
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Select a city</h3>
+              <Nav
+                bsStyle="pills"
+                stacked
+                activeKey={activePlace}
+                onSelect={index => {
+                  this.setState({ activePlace: index });
+                }}
+              >
+                {PLACES.map((place, index) => (
+                  <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                ))}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={activePlace} city={PLACES[activePlace].city} />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default App;
